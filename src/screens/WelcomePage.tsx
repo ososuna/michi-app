d17545d5a0a90d0ac0e38ad250f58ebbff68a120
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2023-04-27 16:21:33 -0600

    get started button

--- src/screens/WelcomePage.tsx
+++ src/screens/WelcomePage.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { Image, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-ionicons';
 import { colors } from '../theme/theme';
 
 export const WelcomePage = () => {
@@ -9,10 +10,16 @@ export const WelcomePage = () => {
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ fontSize: 22 }}>Welcome to the Michi App</Text>
         <Image style={{ width: 260, height: 260 }} source={require('../assets/img/cat.png')} />
-        <TouchableOpacity style={{ backgroundColor: colors.primary, borderRadius: 8, width: 140, alignItems: 'center', marginTop: 20, maxHeight: 48 }}>
-          <Text style={{ color: 'white', fontSize: 18, paddingHorizontal: 25, paddingVertical: 14 }}>
-            Get started
-          </Text>
+        <TouchableOpacity style={{ backgroundColor: colors.primary, borderRadius: 8, width: 160, height: 48, alignItems: 'center', marginTop: 20, maxHeight: 48, flexDirection: 'row' }}>
+          <View style={{ flex: 9 }}>
+            <Text style={{ color: 'white', fontSize: 18, textAlign: 'right' }}>
+              Get started
+            </Text>
+          </View>
+          <View style={{ flex: 1 }} />
+          <View style={{ flex: 3, alignItems: 'flex-start' }}>
+            <Icon name="arrow-round-forward" color="white" />
+          </View>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
