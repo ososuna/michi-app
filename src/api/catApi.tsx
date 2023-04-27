e3e5f7716d277656cab3b379ba1e0bff52ab7da3
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2023-04-27 17:02:30 -0600

    cat api

--- src/api/catApi.tsx
+++ src/api/catApi.tsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
-import { API_URL } from '@env';
+import { API_URL, API_KEY } from '@env';
 
 const catApi = axios.create({
   baseURL: API_URL,
+  headers: {
+    'x-api-key': API_KEY,
+  },
 });
 
 export default catApi;
